8b2331db1115f7924df0bce3bda52eba13fd2950
Divya Joshi <divyaj.redj@gmail.com>
2025-03-16 20:09:37 -0400

    Task 1 - Base Structure Setup.

---
+++ cc_15.js
@@ -0,0 +1,3 @@
+//Task 1: Creating the Base Structure 
+const riskDashboard = document.getElementById("riskDashboard"); //selecting container element
+console.log ("risk dashboard loaded"); //printing message to console
